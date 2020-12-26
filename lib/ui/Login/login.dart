874445db19107b806ba9cb41139709094cdd0d29
elif parlak <elf.prlk64@gmail.com>
2020-12-26 17:43:37 +0300

    login page geldi

--- lib/ui/Login/login.dart
+++ lib/ui/Login/login.dart
@@ -1,214 +1,187 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:stuventmobil/app/exceptions.dart';
-import 'package:stuventmobil/common_widget/platform_duyarli_alert_dialog.dart';
-import 'package:stuventmobil/common_widget/social_log_in_button.dart';
-import 'package:stuventmobil/ui/Login/new_User.dart';
-import 'package:stuventmobil/viewmodel/user_model.dart';
+import '../const.dart';
 
-class Login extends StatefulWidget {
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  final formKey = GlobalKey<FormState>();
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  String mail, password;
-  String result = "";
-  bool otomatikKontrol = false;
-
+class LoginPage extends StatelessWidget {
+  static GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  static Key key1 = new GlobalKey();
+  static Key key2 = new GlobalKey();
+  Size size;
   @override
   Widget build(BuildContext context) {
-    UserModel _userModel = Provider.of<UserModel>(context);
-    return Theme(
-        data: Theme.of(context).copyWith(
-          accentColor: Colors.green,
-          hintColor: Colors.indigo,
-          errorColor: Colors.red,
-          //primaryColor: Colors.teal),
-        ),
-        child: Scaffold(
-          key: _scaffoldKey,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              emailvesifregiris();
-            },
-            child: Icon(Icons.arrow_forward),
-          ),
-          backgroundColor: Colors.grey.shade200,
-          appBar: AppBar(
-            title: Center(
-                child: Text(
-              "Stuvent Etkinlik Habercisi",
-              style: TextStyle(fontSize: 25),
-            )),
-          ),
-          body: Padding(
-            padding: EdgeInsets.all(10),
-            child: Form(
-              key: formKey,
-              child: ListView(children: <Widget>[
-                Center(
-                    child: Text(
-                  "Üye E-posta ve Şifre ile Giriş",
-                  style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.teal),
-                )),
-                SizedBox(
-                  height: 25,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.mail),
-                    hintText: "E-posta",
-                    hintStyle: TextStyle(fontSize: 12),
-                    labelText: "E-posta adresiniz",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: _emailKontrol,
-                  onSaved: (String value) => mail = value,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: "Şifre",
-                    hintStyle: TextStyle(fontSize: 12),
-                    labelText: "Şifreniz",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (String value) {
-                    if (value.length < 6) {
-                      return "En az 6 karakter gerekli";
-                    }
-                    return null;
-                  },
-                  onSaved: (String value) => password = value,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SocialLoginButton(
-                  butonText: "Şifremi Unuttum",
-                  textColor: Colors.yellow,
-                  butonColor: Colors.purple,
-                  onPressed: () {
-                    _sifremiUnuttum(_userModel);
-                  },
-                  butonIcon: Icon(Icons.lock_open),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SocialLoginButton(
-                  butonText: "Yeni Kullanıcı Oluştur",
-                  butonColor: Colors.blue,
-                  onPressed: (() => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => NewUser()))),
-                  butonIcon: Icon(
-                    Icons.account_circle,
-                    size: 35,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SocialLoginButton(
-                  butonText: "Google ile Giriş",
-                  textColor: Colors.black,
-                  butonColor: Colors.white,
-                  onPressed: () {
-                    gSignIn(context);
-                  },
-                  butonIcon: Image.asset("images/google-logo.png"),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
+    size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: BoxDecoration(gradient: loginPageBg),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildHeader(),
                 Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: Text(
-                    result,
-                    textAlign: TextAlign.center,
-                  ),
-                )
-              ]),
+                  padding: const EdgeInsets.only(),
+                  child: Theme(
+                      data: ThemeData(primaryColor: Colors.indigo.shade200),
+                      child: buildFormfield()),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                googleButton(),
+                SizedBox(
+                  height: 10,
+                ),
+                hesapOlustur()
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 
-  Future<void> _sifremiUnuttum(UserModel userModel) async {
-    formKey.currentState.save();
-    if (mail == "") {
-      setState(() {
-        result = "E-posta alanını boş bırakamazsınız";
-      });
-    } else {
-      try {
-        bool sonuc = await userModel.sendPasswordResetEmail(mail);
-        if (sonuc == true || sonuc == null) {
-          PlatformDuyarliAlertDialog(
-            baslik: "Şifre Sıfırlama Mailı Gönderildi",
-            icerik:
-                "Şifre sıfırlama mailı başarılı bir şekilde $mail adresine gönderildi",
-            anaButonYazisi: "Tamam",
-          ).goster(context);
-        } else {
-          PlatformDuyarliAlertDialog(
-            baslik: "Şifre Sıfırlama Mailı Gönderilemedi :(",
-            icerik: "Şifre sıfırlama mailı gönderilirken bir sorun oluştu.\n" +
-                "İnternet bağlantınızı kontrol edin.",
-            anaButonYazisi: "Tamam",
-          ).goster(context);
-        }
-      } on PlatformException catch (e) {
-        PlatformDuyarliAlertDialog(
-          baslik: "Şifre Sıfırlama Mailı HATA",
-          icerik: Exceptions.goster(e.code),
-          anaButonYazisi: "Tamam",
-        ).goster(context);
-      }
-    }
+  Widget buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: Container(
+          height: size.height * 0.29,
+          width: size.width * 0.86,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Stuvent Etkinlik Habercisi",
+                style: headerText,
+              ),
+              Text(
+                "Devam etmek için giriş yapın",
+                style: miniHeader,
+              )
+            ],
+          )),
+    );
   }
 
-  String _emailKontrol(String value) {
-    Pattern pattern =
-        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-    RegExp regex = new RegExp(pattern);
-    if (!regex.hasMatch(value))
-      return 'Geçersiz mail';
-    else
-      return null;
+  Widget buildFormfield() {
+    return Container(
+      height: size.height * 0.39,
+      width: size.width * 0.86,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0, right: 30, left: 30),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            TextFormField(
+              style: TextStyle(color: Colors.indigo.shade200),
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.mail,
+                  color: Colors.indigo.shade200,
+                ),
+                hintStyle: TextStyle(
+                    fontFamily: "Catamaran",
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo.shade200,
+                    fontSize: 17),
+                hintText: "E-Posta Adresi",
+              ),
+              key: key1,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              style: TextStyle(color: Colors.indigo.shade200),
+              key: key2,
+              decoration: InputDecoration(
+                hintText: "Şifre",
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.indigo.shade200,
+                ),
+                hintStyle: TextStyle(
+                    fontFamily: "Catamaran",
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo.shade200,
+                    fontSize: 17),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  child: Text(
+                    "Şifreni mi unuttun?",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "Catamaran",
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigo.shade200),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        gradient: indigoButton,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Icon(Icons.forward, color: Colors.white))
+              ],
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 
-  Future<void> gSignIn(BuildContext context) async {
-    final _userModel = Provider.of<UserModel>(context, listen: false);
-    await _userModel.signInWithGoogle();
+  Widget googleButton() {
+    return Container(
+      height: 45,
+      width: 170,
+      decoration: BoxDecoration(
+          color: Color.fromRGBO(124, 127, 225, 1),
+          borderRadius: BorderRadius.circular(18)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Google",
+            style: TextStyle(
+                color: Colors.white, fontFamily: "Catamaran", fontSize: 20),
+          ),
+        ],
+      ),
+    );
   }
 
-  Future<void> emailvesifregiris() async {
-    if (formKey.currentState.validate()) {
-      formKey.currentState.save();
-      setState(() {
-        result = "Giriş Yapılıyor...";
-      });
-      final _userModel = Provider.of<UserModel>(context, listen: false);
-      try {
-        await _userModel.signInWithEmailandPassword(mail, password);
-      } catch (e) {
-        debugPrint("emailvesifregiris Hata: " + e.toString());
-      }
-    }
+  Widget hesapOlustur() {
+    return Center(
+      child: Container(
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "Hesap Oluştur",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
